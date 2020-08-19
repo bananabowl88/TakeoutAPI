139fa308fca52adf96dd69c18e3be53f8e9b278e
bananabowl88 <v.smita123@gmail.com>
2020-08-19 16:03:23 -0700

    added random restaurant generation and added to gitignore

--- web_server.py
+++ web_server.py
@@ -1,4 +1,6 @@
 import yelp_request
+import json
+import logging
 from flask import Flask
 app = Flask(__name__)
 
@@ -19,15 +21,24 @@ def restaurant():
     if "location" in args:
         location = args.get('location')
 
-        return yelp_request.get_yelp_loc(location)
+        try:
+            return yelp_request.get_yelp_loc(location)
+
+        except Exception as e:
+            logging.exception(e)
+            return "Internal Server Error", 500
+
 
     elif "longitude" in args and "latitude" in args:
         longitude = args["longitude"]
         latitude = args["latitude"]
 
         # num = args.get('num')
-        return yelp_request.get_yelp_rad(longitude, latitude, radius=4000)
-
+        try:
+            return yelp_request.get_yelp_rad(longitude, latitude, radius=4000)
+        except Exception as e:
+            logging.exception(e)
+            return "Internal Server Error", 500
 
     elif "location" not in args:
         return "Error, did not return valid location", 400
@@ -35,28 +46,44 @@ def restaurant():
     elif  "longitude" not in args or "latitude" not in args:
         return "Error, did not return valid longitude or latitude", 400
 
+#random restaurant (parses thru random restaurant)
 
-
-
-
-@app.route('/rest')
-def rest():
+@app.route('/random')
+def random():
     args = request.args
+    text = ""
 
-    if "longitude" not in args or "latitude" not in args:
+    if "location" in args:
+        location = args.get('location')
+        try:
+            return yelp_request.randomRestaurantLoc(location)
+
+        except Exception as e:
+            logging.exception(e)
+            return "Internal Server Error", 500
+
+        #text =  yelp_request.get_yelp_loc(location)
+
+    elif "longitude" in args and "latitude" in args:
+        longitude = args["longitude"]
+        latitude = args["latitude"]
+        try:
+            return yelp_request.randomRestaurantRad(longitude, latitude, radius=4000)
+
+        except Exception as e:
+            logging.exception(e)
+            return "Internal Server Error", 500
+        #text= yelp_request.get_yelp_rad(longitude, latitude, radius=4000)
+
+    elif "location" not in args:
+        return "Error, did not return valid location", 400
+
+    elif "longitude" not in args or "latitude" not in args:
         return "Error, did not return valid longitude or latitude", 400
 
-    longitude = args["longitude"]
-    latitude = args["latitude"]
 
-
-
-    #num = args.get('num')
-    return yelp_request.get_yelp_rad(longitude, latitude, radius=4000)
-    #'Your longitude is ' + longitude + ' and your latitude is ' + latitude
-
-
-
+   # parsed_json = (json.loads(text))
+   # print(json.dumps(parsed_json, indent=4, sort_keys = True))
 
 
 @app.route("/")
--- yelp_request.py
+++ yelp_request.py
@@ -1,11 +1,15 @@
 import requests
-from flask import Flask, render_template
+import json
+import random
 
 
-app = Flask(__name__)
+#yelp_api_key = "UcIJVAd9xi0V1fFZp3bAA2AlxZWaAayRlSwTYyaSQ4JaEK_vTkeNr5_LSBW8AZrDLfnVybv1BSXXHWPCdfsecQOa4cQlqeRnrRNGW3LEloFv0ZVtH2SMffBztAtMW3Yx"
+with open('yelp_api_credentials.json') as file:
+    apiData = json.load(file)
+    apiKey = apiData["yelp_api_key"]
+    header = {'Authorization': 'Bearer ' + str(apiKey)}
 
-yelp_api_key = "UcIJVAd9xi0V1fFZp3bAA2AlxZWaAayRlSwTYyaSQ4JaEK_vTkeNr5_LSBW8AZrDLfnVybv1BSXXHWPCdfsecQOa4cQlqeRnrRNGW3LEloFv0ZVtH2SMffBztAtMW3Yx"
-header = {'Authorization': 'Bearer UcIJVAd9xi0V1fFZp3bAA2AlxZWaAayRlSwTYyaSQ4JaEK_vTkeNr5_LSBW8AZrDLfnVybv1BSXXHWPCdfsecQOa4cQlqeRnrRNGW3LEloFv0ZVtH2SMffBztAtMW3Yx'}
+#header = {'Authorization': 'Bearer UcIJVAd9xi0V1fFZp3bAA2AlxZWaAayRlSwTYyaSQ4JaEK_vTkeNr5_LSBW8AZrDLfnVybv1BSXXHWPCdfsecQOa4cQlqeRnrRNGW3LEloFv0ZVtH2SMffBztAtMW3Yx'}
 
 def get_yelp_loc(location):
     payload = {'term': 'food', 'location' : location}
@@ -15,8 +19,38 @@ def get_yelp_loc(location):
 
 def get_yelp_rad(longitude, latitude, radius):
     payload = {"term": 'food', 'longitude': longitude, 'latitude': latitude, 'radius': radius}
+
     r = requests.get("https://api.yelp.com/v3/businesses/search",  params=payload, headers = header)
     return r.text
+
+def randomRestaurantLoc(location):
+    text = get_yelp_loc(location)
+    data = json.loads(text)
+    randNum = random.randint(0,len(data["businesses"]))
+    #print(data["businesses"][randNum]["name"])
+    return data["businesses"][randNum]
+
+def randomRestaurantRad(longitude, latitude, radius):
+    text = get_yelp_rad(longitude, latitude, radius)
+    data = json.loads(text)
+    randNum = random.randint(0, len(data["businesses"]))
+    # print(data["businesses"][randNum]["name"])
+    return data["businesses"][randNum]
+
+
+
+if __name__ == "__main__":
+    print("Executed when invoked directly")
+    #print(get_yelp_loc("NYC"))
+    #text = get_yelp_loc("NYC")
+    #data = json.loads(text)
+   # for i in data["businesses"]:
+     #   print(i["name"])
+
+    randomRestaurantLoc("NYC")
+    #print(data["businesses"][0]["name"])
+    #print(text[0])
+
 
 #print(r.url)
 
